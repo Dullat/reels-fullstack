ce886f4c9372dc01b0fb2431b8aeac2b9d6d8218
dullat <142017979+Dullat@users.noreply.github.com>
2025-09-06 18:03:28 +0530

    pre save hook: run only when password is modified otherwise dont, so that it doest run on normal update in profile like changing avatar

--- backend/src/models/foodpartner.model.js
+++ backend/src/models/foodpartner.model.js
@@ -36,6 +36,7 @@ const foodPartnerSchema = new mongoose.Schema(
 );
 
 foodPartnerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
